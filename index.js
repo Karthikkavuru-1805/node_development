68dda4287b497fd7c2123df28ec9f528d521659c
venkateshd8 <venkateshssd8@gmail.com>
2025-05-11 16:06:28 +0530

    node changes

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (request, response) => {
+  let date = new Date()
+  response.send(`Today date is ${date}`);
+});
+
+const port = 3000;
+app.listen(port);
+console.log(`server is runnning at ${port}`);
+
+
